e596de97d8cbd44cde4e0f88848e4286b341fc23
hoaingodong <hoaingodong1@gmail.com>
2023-04-11 10:25:54 +0700

    fix: fix courseinfo - refractor component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,71 +1,73 @@
-import React from 'react';
+import React from "react";
 
-const Header = (props) => {
-  console.log(props);
-	return (
-		<div>
-			<h1>{props.course}</h1>
-		</div>
-	)
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  );
 };
 
-const Content = (props) => {
-  console.log(props);
-	const lists = props.parts.map(function(item) {
-		return (
-			<div>
-				<p> {item.name} - {item.exercises}</p>
-			</div>
-		)
-	})
-	
-	return lists
+const Part = ({part}) => {
+  return (
+    <div>
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    </div>
+  );
 };
 
-const Total = (props) => {
-  console.log(props);
-	var total = 0
-	
-	props.parts.map(function(item) {	
-		total = total + item.exercises 
-	})
-	
-  
-	return (
-		<div>
-			<p>Number of exercises: {total} </p>
-		</div>
-	)
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map((item) => (
+        <Part part={item} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  var total = 0;
+
+  parts.map(function (item) {
+    total = total + item.exercises;
+  });
+
+  return (
+    <div>
+      <p>Number of exercises: {total} </p>
+    </div>
+  );
 };
 
 const App = () => {
-	const course = {
-		name: 'Half Stack application development',
-		parts: [
-			{
-				name: 'Fundamentals of React',
-				exercises: 10
-			},
-			{
-				name: 'Using props to pass data',
-				exercises: 7
-			},
-			{
-				name: 'State of a component',
-				exercises: 14
-			}
-		]
-	}
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
-	return (
-		<div>
-			<h1>{course.name}</h1>
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
-		</div>
-	)
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
 };
-
-
 
 export default App;
