e35a49f9ef8002cf1cea2198f8aea2e6957e2066
hoaingodong <hoaingodong1@gmail.com>
2023-04-21 15:26:45 +0700

    feat: submit phonebook-backend exercises

--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 var morgan = require('morgan')
+app.use(express.static('build'))
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('build'))
 
-// app.use(morgan('tiny'))
 app.use(morgan(function (tokens, req, res) {
     return [
         tokens.method(req, res),
@@ -106,7 +121,10 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
