7c8eb2ba679440ef86d5be7cdb0a70e4fd898783
hoaingodong <hoaingodong1@gmail.com>
2023-04-26 15:58:10 +0700

    fix: fix validation of phone number

--- part3/PhonebookBackend/models/person.js
+++ part3/PhonebookBackend/models/person.js
@@ -20,7 +20,8 @@ const personSchema = new mongoose.Schema({
         type: String,
         validate: {
             validator: function(v) {
-                return /\d{2}-\d{7}/.test(v);
+                return /\d{2,3}-\d{6,}$/.test(v);
+                // return /\d{2,3}-\d{6}\d+$/.test(v);
             },
             message: props => `${props.value} is not a valid phone number!`
         },
