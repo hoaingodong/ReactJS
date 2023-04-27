b3c5ecbf980a9925b3748bdfd6f9f0b4b7dae17b
hoaingodong <hoaingodong1@gmail.com>
2023-04-27 16:46:32 +0700

    feat: submit 4.3-4.6 exercises

---
+++ part4/bloglist-application/utils/list_helper.js
@@ -0,0 +1,22 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = blogs => {
+    const reducer = (sum, item) =>{
+        return sum+item.likes
+    }
+
+    return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    const likes = blogs.map(e => e.likes)
+    const blog = blogs[likes.indexOf(Math.max(...likes))]
+    console.log(likes);
+    return blog
+}
+
+module.exports = {
+    dummy, totalLikes, favoriteBlog
+}
