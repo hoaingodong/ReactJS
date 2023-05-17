6b6544897d81f0d5cb64d44dc468c36e10ded842
hoaingodong <hoaingodong1@gmail.com>
2023-05-17 15:11:39 +0700

    fix: refactor config variable

--- part4/bloglist-frontend/src/services/blogs.js
+++ part4/bloglist-frontend/src/services/blogs.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 const baseUrl = '/api/blogs'
 
 let token = null
+let config
 
 const setToken = newToken => {
   token = `Bearer ${newToken}`
+  config = {
+    headers: { Authorization: token },
+  }
 }
 
 const getAll = async () => {
@@ -13,27 +17,18 @@ const getAll = async () => {
 }
 
 const create = async newObject => {
-  const config = {
-    headers: { Authorization: token },
-  }
 
   const response = await axios.post(baseUrl, newObject, config)
   return response.data
 }
 
 const update = async (newObject) => {
-  const config = {
-    headers: { Authorization: token },
-  }
 
   const response = await axios.put(`${ baseUrl }/${newObject.id}`, newObject, config)
   return response.data
 }
 
 const remove = async id => {
-  const config = {
-    headers: { Authorization: token },
-  }
 
   const response = await axios.delete(`${baseUrl}/${id}`, config)
   return response.data
