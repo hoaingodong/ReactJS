426aed367b3a5d7b501c5d76b3f91fcec1bc1426
hoaingodong <hoaingodong1@gmail.com>
2023-05-25 15:38:15 +0700

    feat: add 6.14 - 6.15 exercises

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,8 +2,18 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import {useEffect} from "react"
+import noteAnecdote from './services/anecdotes'
 
 const App = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        noteAnecdote
+            .getAll().then(notes => dispatch(setAnecdotes(notes)))
+    }, [dispatch])
+
     return (
         <div>
             <h2>Anecdotes</h2>
