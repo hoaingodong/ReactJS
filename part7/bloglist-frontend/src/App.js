740cd5702b8aa42029d87b2072b5bbd959a0646f
hoaingodong <hoaingodong1@gmail.com>
2023-06-01 16:44:59 +0700

    feat: add 7.17 ex - create a navigation menu for the application

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -13,13 +13,13 @@ import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
 import UserDetail from './components/UserDetail'
 import BlogDetail from './components/BlogDetail'
+import Menu from './components/Menu'
 
 const App = () => {
   const dispatch = useDispatch()
   const user = useSelector(state => state.user)
   const users = useSelector(state => state.users)
   const blogs = useSelector(state => state.blogs)
-
 
   useEffect(() => {
     dispatch(initializeUser()),
@@ -52,6 +52,7 @@ const App = () => {
         <h2>blogs</h2>
         <Notification></Notification>
         <p>Welcome {user.username} </p>
+        <Menu></Menu>
         <button onClick={() => handleLogout()}>Logout</button>
       </div>
       <Routes>
@@ -59,6 +60,7 @@ const App = () => {
         <Route path="/users/:id"element={<UserDetail users={users}/>}> </Route>
         <Route path="/blogs/:id" element={<BlogDetail blogs={blogs}/>} />
         <Route path="/" element={<Home/>} />
+        <Route path="/blogs" element={<Home/>} />
       </Routes>
     </Router>
   )
